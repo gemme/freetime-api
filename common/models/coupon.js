ca17be0cbb4d324bc21a3ec5d5761854ec527520
Carlos Barragan <cbarragan@itexico.net>
2018-04-19 15:50:09 -0500

    Coupon/allByCategory endpoint created to return category of the coupons

--- common/models/coupon.js
+++ common/models/coupon.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Coupon) {
-
+  Coupon.allByCategory = function(cb) {
+    Coupon.find({
+      'include': {
+        'relation': 'account',
+        'scope': {
+          'fields': ['category', 'id'],
+        },
+      },
+    }).then(coupons => {
+      return cb(null, coupons);
+    })
+    .catch(err => cb(err));
+  };
+  Coupon.remoteMethod(
+        'allByCategory', {
+          http: {
+            path: '/allByCategory',
+            verb: 'get',
+          },
+          returns: {
+            arg: 'coupons',
+            type: 'string',
+          },
+        }
+      );
 };
